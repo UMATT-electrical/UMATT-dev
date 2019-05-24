2a64522cb342c39611abdde9195d480126e926d2
Keaton Cross <keatoncross6@gmail.com>
2019-05-23 19:19:25 -0500

    Added utility to make new label with image

--- widgets.py
+++ widgets.py
@@ -1,5 +1,6 @@
 from PyQt4 import QtGui, QtCore
 import compassWidget2 as CW
+from constants import RGBStrings
 
 
 def make_pixmap(parent, relative_file, size, position=None):
@@ -63,9 +64,24 @@ def make_label(
         label.setStyleSheet(style_sheet)
     if font:
         label.setFont(font)
-    label.setAlignment(alignment)
+    if alignment:
+        label.setAlignment(alignment)
     if size:
         label.resize(*size)
     if position:
         label.move(*position)
     return label
+
+
+def make_label_with_image_background(
+        parent, size, position, image_name, text, background_colour, font, text_colour, margin_width=5):
+    smaller_size = (size[0]-2*margin_width, size[1]-2*margin_width)
+    smaller_position = (position[0]+margin_width, position[1]+margin_width)
+    background_label = make_label(
+        parent, '', size=size, position=position, alignment=None, style_sheet=background_colour)
+    text_background = make_label(parent, '', smaller_size, smaller_position, style_sheet=RGBStrings.WHITE.background_string)
+    image = make_pixmap(parent, image_name, (size[1]*.5, size[1]*.5), (position[0]+size[0]*.2, position[1]+size[1]*.2))
+    text_label = make_label(
+        parent, text, size=(size[0]*.5, size[1]*.5), position=(position[0]+size[0]+.5, position[1]+size[1]*.2),
+        font=font, style_sheet=text_colour)
+    return background_label, text_background, image, text_label

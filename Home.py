326b6bff61c956131c45f11654c58a33b04cb42b
Paul Schreckenbach <paul.schreckenbach@umsats.ca>
2019-05-23 19:51:38 -0500

    fix

--- Home.py
+++ Home.py
@@ -99,7 +99,7 @@ class HomeWindow(QtGui.QWidget):
         self.label_rpm = self.make_label(
             'mode',
             size=(main_menu_item_size * 4, main_menu_item_size),
-            position=(WIDTH - main_menu_item_size *2, 2*(main_menu_item_size + HEIGHT/80) + HEIGHT/160),
+            position=(WIDTH/2 - main_menu_item_size *2, 2*(main_menu_item_size + HEIGHT/80) + HEIGHT/160),
             font=QtGui.QFont('Times', 30, QtGui.QFont.Bold),
             style_sheet='%s; %s'%(RGBStrings.GOLD.background_string, RGBStrings.BLACK.colour_string)
         )
